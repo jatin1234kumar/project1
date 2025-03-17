c47a8d67b7c9d2b9285e1b29834f36b40f138ece
jatin1234kumar <mrjatinkumar97@gmail.com>
2025-03-17 12:39:34 +0530

    made the animation side of the website

--- script.js
+++ script.js
@@ -3,9 +3,25 @@ const navbar = document.getElementById("Navbar");
 window.addEventListener("scroll", () => {
   if (window.scrollY >= 50) {
     navbar.classList.add("sticky");
-    console.log("got sticky");
   } else {
     navbar.classList.remove("sticky");
-    console.log("removed sticky");
+  }
+});
+
+const featureBoxs = document.querySelectorAll(".featureBox");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 559 && window.scrollY < 1039) {
+    showAnimation(0);
+  } else if (window.scrollY >= 1039 && window.scrollY < 1519) {
+    showAnimation(1);
+  } else if (window.scrollY >= 1519 && window.scrollY < 1999) {
+    showAnimation(2);
+  } else if (window.scrollY >= 1999) {
+    showAnimation(3);
+  }
+
+  function showAnimation(value) {
+    featureBoxs[value].classList.add("showAnimation");
   }
 });
