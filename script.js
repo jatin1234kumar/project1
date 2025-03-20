a9a092d5a2ec447957d894c59e5d97370fdcbe16
jatin1234kumar <mrjatinkumar97@gmail.com>
2025-03-20 14:00:26 +0530

    made the project section

--- script.js
+++ script.js
@@ -44,21 +44,3 @@ window.addEventListener("scroll", () => {
     featureBoxs[value].classList.add("showAnimation");
   }
 });
-
-document.addEventListener("DOMContentLoaded", function () {
-  const elements = document.querySelectorAll(".animate");
-
-  const observer = new IntersectionObserver(
-    (entries, observer) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add("visible"); // Add animation
-          observer.unobserve(entry.target); // Stop observing to prevent re-triggering
-        }
-      });
-    },
-    { threshold: 0.1 }
-  ); // Trigger when 10% of the element is visible
-
-  elements.forEach((el) => observer.observe(el));
-});
